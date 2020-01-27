f8f5bff6076c5794b995801ca330a4c285316081
Maciej Kryczka <maciejkryczka@hotmail.com>
2020-01-27 12:18:40 +0100

    add MainLayout component

---
+++ src/components/layout/MainLayout/MainLayout.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+
+import styles from './MainLayout.module.scss';
+
+//import {connect} from 'react-redux';
+//import {reduxSelector, reduxActionCreator} from '../../../redux/example.js';
+
+const Component = ({className, children}) => {
+  return (
+    <div className={clsx(className, styles.root)}>
+      <h2>MainLayout</h2>
+      {children}
+    </div>
+  );
+};
+
+Component.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+
+// const mapStateToProps = state => ({
+//   someProp: reduxSelector(state),
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//   someAction: arg => dispatch(reduxActionCreator(arg),)
+// });
+
+// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+
+export {
+  Component as MainLayout,
+  //Container as MainLayout,
+  Component as MainLayoutComponent,
+};
