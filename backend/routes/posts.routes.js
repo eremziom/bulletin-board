4a56cf324fce8ec1d976895a26179571531dc5f2
Maciej Kryczka <maciejkryczka@hotmail.com>
2020-02-05 12:19:27 +0100

    add server.js and posts endpoints

---
+++ backend/routes/posts.routes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/post.model');
+
+//ENDPOINT FOR ALL POSTS RENDER
+router.get('/posts', async (req, res) => {
+  try {
+    const result = await Post
+    .find()
+    .select('title pubDate local status photo')
+    .sort({pubDate: -1});
+    if(!result) res.status(404).json({ post: 'Not found...'});
+    else res.json(result);
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/posts/:id', async (req, res) => {
+  try {
+    const result = await Post
+    .findById(req.params.id)
+    if(!result) res.status(404).json({ post: 'Not found...'});
+    else res.json(result);
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router
---
+++ backend/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+
+//POSTROUTES IMPORT
+const postRoutes = require('./routes/posts.routes');
+
+const app = express();
+
+//MIDLEWARE
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//API ENDPOINTS
+
+app.use('/api', postRoutes);
+
+//API ERROR PAGE
+
+app.use('/api', (req, res) => {
+  res.status(404).send({ post: 'Not found...'});
+});
+
+//BUILD APP
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+//MONGOOSE
+
+mongoose.connect('mongodb://localhost:27017/bulletinBoard', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('Successfully connected to teh database');
+});
+db.on('error', err => console.log('Error: ' + err));
+
+//START SERVER
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log('Server is running on port : '+ port);
+});
